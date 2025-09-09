c3b124591961d83900708b34563ddaa3a1fcbd4f
Maxim Bayurov <43564756+MaximBayurov@users.noreply.github.com>
2025-09-09 23:12:42 +0300

    HW4: is completed (#5)

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -2,18 +2,61 @@ package hw04lrucache
 
 type Key string
 
+// cacheItem представляет элемент кэша.
+type cacheItem struct {
+	key   Key
+	value any
+}
+
 type Cache interface {
+	// Set добавляет/обновляет значение в кэше по ключу.
+	// Возвращаемое значение - флаг, присутствовал ли элемент в кэше.
 	Set(key Key, value interface{}) bool
+	// Get получение элемента по ключу
 	Get(key Key) (interface{}, bool)
+	// Clear очистка кэша
 	Clear()
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+}
+
+func (c *lruCache) Set(key Key, value any) bool {
+	val, isExists := c.items[key]
+	if isExists {
+		val.Value.(*cacheItem).value = value
+		c.queue.MoveToFront(val)
+
+		return true
+	}
+	if c.queue.Len() == c.capacity {
+		oldest := c.queue.Back()
+		oldestItem := oldest.Value.(*cacheItem)
+		delete(c.items, oldestItem.key)
+		c.queue.Remove(oldest)
+	}
+	c.items[key] = c.queue.PushFront(&cacheItem{
+		key:   key,
+		value: value,
+	})
+	return false
+}
+
+func (c *lruCache) Get(key Key) (any, bool) {
+	val, isExists := c.items[key]
+	if isExists {
+		c.queue.MoveToFront(val)
+		return val.Value.(*cacheItem).value, true
+	}
+	return nil, false
+}
+
+func (c *lruCache) Clear() {
+	c.queue = NewList()
+	c.items = make(map[Key]*ListItem, c.capacity)
 }
 
 func NewCache(capacity int) Cache {
--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -3,6 +3,7 @@ package hw04lrucache
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -44,8 +45,53 @@ func TestList(t *testing.T) {
 
 		elems := make([]int, 0, l.Len())
 		for i := l.Front(); i != nil; i = i.Next {
-			elems = append(elems, i.Value.(int))
+			v, ok := i.Value.(int)
+			if ok {
+				elems = append(elems, v)
+			}
 		}
 		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
+	})
+
+	t.Run("correct links", func(t *testing.T) {
+		l := NewList()
+
+		l.PushFront(10) // [10]
+		l.PushFront(20) // [10, 20]
+		l.PushFront(30) // [10, 20, 30]
+
+		assert.Equal(t, l.Front().Next, l.Back().Prev, "Pushing front")
+
+		l = NewList()
+
+		l.PushBack(10) // [10]
+		l.PushBack(20) // [10, 20]
+		l.PushBack(30) // [10, 20, 30]
+
+		assert.Equal(t, l.Front().Next, l.Back().Prev, "Pushing back")
+
+		l = NewList()
+
+		l.PushBack(10)  // [10]
+		l.PushFront(20) // [10, 20]
+		l.PushBack(30)  // [10, 20, 30]
+
+		assert.Equal(t, l.Front().Next, l.Back().Prev, "Pushing different 1")
+
+		l = NewList()
+
+		l.PushFront(10) // [10]
+		l.PushBack(20)  // [10, 20]
+		l.PushFront(30) // [10, 20, 30]
+
+		assert.Equal(t, l.Front().Next, l.Back().Prev, "Pushing different 2")
+
+		l = NewList()
+
+		l.PushBack(10)  // [10]
+		l.PushBack(20)  // [10, 20]
+		l.PushFront(30) // [10, 20, 30]
+
+		assert.Equal(t, l.Front().Next, l.Back().Prev, "Pushing different 3")
 	})
 }
