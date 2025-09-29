8516589a570cfabb4cdc530be5ccd42469bf7393
Maxim Bayurov <vakakak20@gmail.com>
2025-09-29 14:38:32 +0300

    HW7: fix

--- hw07_file_copying/test.sh
+++ hw07_file_copying/test.sh
@@ -34,15 +34,15 @@ if [ -f $TO ]; then
   exit 1
 fi
 
-./go-cp -from testdata/empty.txt -to $TO
-cmp $TO testdata/out_empty.txt
-
 ln -sf $FROM symlink
 ./go-cp -from symlink -to $TO
 if [ -f $TO ]; then
   exit 1
 fi
 rm -f symlink
+
+./go-cp -from testdata/empty.txt -to $TO
+cmp $TO testdata/out_empty.txt
 
 rm -f go-cp out.txt
 echo "PASS"
