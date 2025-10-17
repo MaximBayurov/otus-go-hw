32375e017a2bc0713ff14ef36b2c990b638a7cec
Maxim Bayurov <43564756+MaximBayurov@users.noreply.github.com>
2025-10-17 16:14:34 +0300

    HW8: is completed (#9)
    * HW8: is completed
    
    * HW8: .sync deleted
    
    * HW8: fix empty files

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,56 @@
 package main
 
+import (
+	"context"
+	"errors"
+	"log"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	command := makeCommandFrom(cmd)
+	setEnvFor(command, env)
+
+	err := command.Run()
+	if err != nil {
+		exitError := &exec.ExitError{}
+		if errors.As(err, &exitError) {
+			return exitError.ExitCode()
+		}
+		log.Println(err)
+	}
+
+	return 0
+}
+
+// makeCommandFrom создаёт команду из переданных параметров.
+func makeCommandFrom(cmd []string) *exec.Cmd {
+	name := cmd[0]
+	arg := cmd[1:]
+	command := exec.CommandContext(context.TODO(), name, arg...)
+
+	command.Stdout = os.Stdout
+	command.Stdin = os.Stdin
+	command.Stderr = os.Stderr
+
+	return command
+}
+
+// setEnvFor устанавливает переменные окружения для команды.
+func setEnvFor(command *exec.Cmd, env Environment) {
+	currentEnv := createEnvFromSlice(os.Environ())
+
+	for name, envValue := range env {
+		if envValue.NeedRemove {
+			_, exists := currentEnv[name]
+			if exists {
+				delete(currentEnv, name)
+			}
+		}
+		currentEnv[name] = envValue
+	}
+
+	command.Env = currentEnv.toSlice()
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,9 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestRunCmd(t *testing.T) {
+func TestRunCmd(_ *testing.T) {
 	// Place your code here
 }
