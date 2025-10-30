377937251b181e0b3c8f53563d5bf843edc67932
Maxim Bayurov <vakakak20@gmail.com>
2025-10-30 19:14:23 +0300

    HW11: is completed

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
@@ -13,9 +16,62 @@ type TelnetClient interface {
 }
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &primitiveTelnetClient{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+type primitiveTelnetClient struct {
+	address    string
+	timeout    time.Duration
+	in         io.ReadCloser
+	out        io.Writer
+	connection net.Conn
+}
+
+func (c *primitiveTelnetClient) Connect() error {
+	var err error
+	c.connection, err = net.DialTimeout("tcp", c.address, c.timeout)
+	return err
+}
+
+func (c *primitiveTelnetClient) Close() error {
+	return c.connection.Close()
+}
+
+func (c *primitiveTelnetClient) Send() error {
+	scanner := bufio.NewScanner(c.in)
+	for scanner.Scan() {
+		text := scanner.Text() + "\n"
+		_, err := c.connection.Write([]byte(text))
+		if err != nil {
+			return err
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (c *primitiveTelnetClient) Receive() error {
+	reader := bufio.NewReader(c.connection)
+	for {
+		data, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				return nil
+			}
+			return err
+		}
+		_, err = fmt.Fprintf(c.out, data)
+		if err != nil {
+			return err
+		}
+	}
+}
