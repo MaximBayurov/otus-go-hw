00e8e2a163395eebc30a19ab0ac4f4b1fcaa2b03
Maxim Bayurov <vakakak20@gmail.com>
2025-10-30 21:08:10 +0300

    HW11: fix

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -72,7 +72,8 @@ func (c *primitiveTelnetClient) Receive() error {
 		data, err := reader.ReadString('\n')
 		if err != nil {
 			if errors.Is(err, io.EOF) {
-				return nil
+				_, err = fmt.Fprint(c.out, data)
+				return err
 			}
 			return err
 		}
